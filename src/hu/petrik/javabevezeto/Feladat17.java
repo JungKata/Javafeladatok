883a6edba666c82395641b8744e2cb6c6c5cd5d7
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-14 22:03:22 +0200

    feladat17 -változok

---
+++ src/hu/petrik/javabevezeto/Feladat17.java
@@ -0,0 +1,15 @@
+package hu.petrik.javabevezeto;
+
+import java.util.Scanner;
+
+public class Feladat17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double [] t1 = new double[5];
+        double [] t2 = new double[5];
+        double [] t3 = new double[3];
+
+        double elem1 = 0;
+        double elem2 = 0;
+    }
+}
