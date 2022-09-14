4139b43c644f1a8f123e841b08e7ef907a5a5419
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-14 21:58:20 +0200

    feladat16 -psvm -kész

--- src/hu/petrik/javabevezeto/feladat16.java
+++ src/hu/petrik/javabevezeto/feladat16.java
@@ -1,7 +1,24 @@
 package hu.petrik.javabevezeto;
 
+import java.util.Scanner;
+
 public class feladat16 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int [] arr = new int[5];
+        int bekertSzam = 0;
+        int tombSzamok = 0;
+
+        while (tombSzamok < 5){
+            System.out.println("Kérem adjon meg számokat: ");
+            bekertSzam = Integer.parseInt(sc.nextLine());
+            arr[tombSzamok] = bekertSzam;
+            tombSzamok++;
+        }
+        for (int i = 0; i <arr.length; i++) {
+            System.out.printf("Tömb számai: %d\n", arr[i]);
+
+        }
     }
 }
